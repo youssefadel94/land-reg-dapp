da6f43077ac6d15e118aff13995ffdf80ad9d3fc
youssefadel94 <darkmetalxdragon@hotmail.com>
2020-11-08 13:21:35 +0200

    first working test deploy

---
+++ contracts/Payment.sol
@@ -0,0 +1,38 @@
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Payment {
+    address payable public transferFrom;
+    address payable public transferTo;
+    uint256 public paymentAmount;
+
+    constructor() public {
+        transferFrom = msg.sender;
+    }
+
+    event TransferFundEvent(
+        address _transferTo,
+        address _transferFrom,
+        uint256 amount
+    );
+    event CreateUserEvent(uint256 _id);
+
+    function transferFund(address payable _transferTo)
+        public
+        payable
+        returns (bool)
+    {
+        transferTo = _transferTo;
+        transferTo.transfer(msg.value);
+        emit TransferFundEvent(transferTo, transferFrom, msg.value);
+        return true;
+    }
+
+    function getBalanceOfCurrentAccount() public payable returns (uint256) {
+        return transferFrom.balance;
+    }
+
+    function createUser(uint256 _id) public returns (bool) {
+        emit CreateUserEvent (_id);
+        return true;
+    }
+}
