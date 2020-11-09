b77ac34796c5ae029c5b1f7e0656347f5f6f9790
youssefadel94 <darkmetalxdragon@hotmail.com>
2020-11-09 11:52:34 +0200

    basic user loging and contract emit

--- contracts/landReg.sol
+++ contracts/landReg.sol
@@ -1,10 +1,9 @@
 pragma solidity >=0.4.22 <0.8.0;
 
-
 contract LandReg {
     //is Ownable{
     address public owner;
-    mapping(uint256 => bool) public users;
+    mapping(uint256 => uint256) public users;
     //mapping(address => mapping(address => bool)) private ;
 
     //log
@@ -14,17 +13,39 @@ contract LandReg {
 
     event CreateUserEvent(uint256 _id);
 
+    event SendSomethingEvent(
+        uint256 sender,
+        uint256 receiver,
+        fixed128x18 long,
+        fixed128x18 lat
+    );
+
     constructor() public {
         owner = msg.sender;
     }
 
     function createUser(uint256 _id) public returns (bool) {
+        require(users[_id] == 0, "user already registered");
+        users[_id] = _id;
         emit CreateUserEvent(_id);
         return true;
     }
 
-    function isUserReg(uint256 _id) public returns (bool) {
-        emit LogBool(users[_id]);
+    // function isUserReg(uint256 _id) private returns (bool) {
+    //     uint256 state = users[_id];
+    //     //emit LogUint256(state);
+    //     return state == 0;
+    // }
+
+    //function send test
+    function sendSomething(
+        uint256 sender,
+        uint256 receiver,
+        fixed128x18 long,
+        fixed128x18 lat
+    ) public returns (bool) {
+        require(users[receiver] != 0, "receiver is not registered");
+        emit SendSomethingEvent(sender, receiver, long, lat);
         return true;
     }
 
@@ -32,7 +53,7 @@ contract LandReg {
     //     public
     //     returns (bool)
     // {
-        
+
     //     return true;
     //     }
 }
