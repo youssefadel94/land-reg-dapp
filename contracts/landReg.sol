5ddbcad84de4dce608e7bbbb20b5e22f3c4335e2
youssefadel94 <darkmetalxdragon@hotmail.com>
2020-11-09 10:30:42 +0200

    remove log

--- contracts/landReg.sol
+++ contracts/landReg.sol
@@ -1,13 +1,16 @@
 pragma solidity >=0.4.22 <0.8.0;
 
-import "@nomiclabs/buidler/console.sol";
-
 
 contract Payment {
     //is Ownable{
     address public owner;
     mapping(uint256 => bool) public users;
     //mapping(address => mapping(address => bool)) private ;
+
+    //log
+    event LogBool(bool state);
+    event LogUint256(uint256 state);
+    event LogFixed128x18(fixed128x18 state);
 
     event CreateUserEvent(uint256 _id);
 
@@ -21,12 +24,15 @@ contract Payment {
     }
 
     function isUserReg(uint256 _id) public returns (bool) {
-        console.log(users[_id]);
-        return true
+        
+        return true;
     }
 
-    function registerLand(fixed128x18 long, fixed128x18 lat)
-        public
-        returns (bool)
-    {}
+    // function registerLand(fixed128x18 long, fixed128x18 lat)
+    //     public
+    //     returns (bool)
+    // {
+        
+    //     return true;
+    //     }
 }
