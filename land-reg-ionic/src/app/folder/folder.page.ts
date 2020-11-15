25393c0a321447166a7c807826704671c7e67fd2
youssefadel94 <darkmetalxdragon@hotmail.com>
2020-11-15 18:49:29 +0200

    getting web3 to work attempt 1

--- land-reg-ionic/src/app/folder/folder.page.ts
+++ land-reg-ionic/src/app/folder/folder.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { EthcontractService } from '../ethcontract.service';
 
 @Component({
   selector: 'app-folder',
@@ -9,10 +10,44 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
   public folder: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private ethcontractService: EthcontractService) { }
+
+  title = 'your first DApp in Angular';
+  accounts: any;
+  transferFrom = '0x0';
+  balance = '0 ETH';
+  transferTo = '';
+  amount = 0;
+  remarks = '';
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
   }
+  initAndDisplayAccount = () => {
+    let that = this;
+    this.ethcontractService.getAccountInfo().then(function (acctInfo: any) {
+      console.log(acctInfo);
+      that.transferFrom = acctInfo.fromAccount;
+      that.balance = acctInfo.balance;
+    }).catch(function (error) {
+      console.log(error);
+    });
 
+  };
+
+  transferEther(event) {
+    let that = this;
+    console.log(this.transferTo);
+    this.ethcontractService.transferEther(
+      this.transferFrom,
+      this.transferTo,
+      this.amount,
+      this.remarks
+    ).then(function () {
+      that.initAndDisplayAccount();
+    }).catch(function (error) {
+      console.log(error);
+      that.initAndDisplayAccount();
+    })
+  }
 }
