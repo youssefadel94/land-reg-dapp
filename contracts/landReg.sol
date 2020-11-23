367192f97b9ff0109aa298a7b17b8e919aa512fe
youssefadel94 <darkmetalxdragon@hotmail.com>
2020-11-23 12:16:27 +0200

    add adnroid, update land reg

--- contracts/landReg.sol
+++ contracts/landReg.sol
@@ -1,9 +1,21 @@
 pragma solidity >=0.4.22 <0.8.0;
 
+
 contract LandReg {
     //is Ownable{
+    
+    // struct Data {  
+    //     string name;  
+    //     string long;
+    //     string lat; 
+    //     string owner;
+    // }
+
     address public owner;
-    mapping(uint256 => uint256) public users;
+
+    mapping(uint256 => address) public users;
+    //mapping(address => mapping(uint256 => data)) public property;
+    mapping(address => mapping(uint256 => bool)) public propertyFireBase;
     //mapping(address => mapping(address => bool)) private ;
 
     //log
@@ -13,50 +25,64 @@ contract LandReg {
 
     event CreateUserEvent(uint256 _id);
 
-    event transferPropertyEvent(
+    event DeclarePropertyEvent(
+        uint256 sender,
+        uint256 value
+    );
+
+    event TransferPropertyEvent(
         uint256 sender,
         uint256 receiver,
-        uint256 value//,
-        // fixed128x18 long,
-        // fixed128x18 lat
+        uint256 value
     );
 
     constructor() public {
-        owner = msg.sender;
+        owner = (address(this));//msg.sender;//make contract owner of itself
     }
 
-    function createUser(uint256 _id) public returns (bool) {
-        //require(users[_id] == 0, "user already registered");
-        users[_id] = _id;
-        emit CreateUserEvent(_id);
+    // function createUser(uint256 _id, address _address) public returns (bool) {
+    //     //require(users[_id] == _address, "user already registered");
+    //     users[_id] = _id;
+    //     emit CreateUserEvent(_id);
+    //     return true;
+    // }
+
+    function enterUser(uint256 _id, address _address) public returns (bool) {
+        require(users[_id] == address(0), "user already registered");
+        users[_id] = _address;
+        //emit CreateUserEvent(_id);
         return true;
     }
-
     // function isUserReg(uint256 _id) private returns (bool) {
     //     uint256 state = users[_id];
     //     //emit LogUint256(state);
     //     return state == 0;
     // }
+    //function registerLand test
+    function registerLand(
+        uint256 sender,
+        uint256 value
+    ) public returns (bool) {
+        require(users[sender] != address(0), "receiver is not registered");
+        require(!(propertyFireBase[users[sender]][value]), "you own this property");
+        //delete propertyFireBase[users[sender]][value];
+        propertyFireBase[users[sender]][value] = true;
+        emit DeclarePropertyEvent(sender, value);//, long, lat);
+        return true;
+    }
 
     //function send test
     function transferProperty(
         uint256 sender,
         uint256 receiver,
         uint256 value
-        //,
-        // fixed128x18 long,
-        // fixed128x18 lat
     ) public returns (bool) {
-        require(users[receiver] != 0, "receiver is not registered");
-        emit transferPropertyEvent(sender, receiver, value);//, long, lat);
+        require(users[receiver] != address(0), "receiver is not registered");
+        require(propertyFireBase[users[sender]][value] , "you do not own this property");
+        delete propertyFireBase[users[sender]][value];
+        propertyFireBase[users[receiver]][value] = true;
+        emit TransferPropertyEvent(sender, receiver, value);//, long, lat);
         return true;
     }
 
-    // function registerLand(fixed128x18 long, fixed128x18 lat)
-    //     public
-    //     returns (bool)
-    // {
-
-    //     return true;
-    //     }
 }
